a65153ad5491af516b99e9aa9fbadb5988fc6b60
shicks <shicks255@yahoo.com>
2022-11-16 20:31:08 -0600

    wepabk

--- tools/webpack/src/indexOld.js
+++ tools/webpack/src/indexOld.js
@@ -1,11 +1,7 @@
-import _ from 'lodash';
-
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'World'], ' ' );
-
-    // element.innerHTML = "jljkkl"
 
     return element;
 }
